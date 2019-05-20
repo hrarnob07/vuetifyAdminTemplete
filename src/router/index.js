93506bd3c0babc0c13edc1c20f8da318c30b7599
hrarnob07 <hrarnob07@gmail.com>
2019-05-20 17:42:57 +0600

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Dashboard from '../pages/Dashboard.vue';
+import Login from '../pages/core/Login.vue';
+// import Error from '../pages/core/Error.vue';
+
+
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: Dashboard,
+      meta: {
+        breadcrumb: [
+          { name: 'Dashboard' }
+        ]
+      }
+    },
+    
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      meta: {
+        allowAnonymous: true
+      }
+    },
+    
+  ]
+});
